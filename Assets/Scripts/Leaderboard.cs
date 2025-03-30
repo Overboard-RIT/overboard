bd52efede8db07d512c858dfcfa04075a3819c18
Elia Cohen <mrc6113@rit.edu>
2025-03-29 20:59:19 -0400

    leaderboard objects

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Leaderboard : MonoBehaviour
+{
+    public struct LeaderboardEntry
+    {
+        public LeaderboardEntry(string id, string playerName, int score)
+        {
+            this.id = id;
+            this.playerName = playerName;
+            this.score = score;
+        }
+        public string id;
+        public string playerName;
+        public int score;
+    }
+
+    public class LeaderboardList : IList<LeaderboardEntry>
+    {
+        // entries
+        private List<LeaderboardEntry> entries = new List<LeaderboardEntry>();
+        private int maxEntries;
+        public int Count => entries.Count;
+        public bool IsReadOnly => false;
+
+        public LeaderboardEntry this[int index]
+        {
+            get => entries[index];
+            set => throw new System.NotSupportedException("Set is not supported");
+        }
+        public void Add(LeaderboardEntry entry)
+        {
+            // if the leaderboard is full and the new entry is lower than the lowest score, return out
+            if (entries.Count >= maxEntries && entry.score < entries.Last().score)
+                return;
+            // if player is already on the leaderboard, keep the higher score
+            for (int i = 0; i < entries.Count; i++)
+            {
+                if (entries[i].id == entry.id)
+                {
+                    if (entry.score > entries[i].score)
+                    {
+                        entries[i] = entry;
+                        entries.Sort((e1, e2) => e2.score.CompareTo(e1.score));
+                        return;
+                    }
+                }
+            }
+            // if the leaderboard is not full, add the entry
+            if (entries.Count < maxEntries)
+            {
+                entries.Add(entry);
+                entries.Sort((e1, e2) => e2.score.CompareTo(e1.score));
+                return;
+            }
+            // leaderboard is full, player not already on, see if player made the leaderboard
+            if (entry.score < entries.Last().score) return;
+            entries.Add(entry);
+            entries.Sort((e1, e2) => e2.score.CompareTo(e1.score));
+            return;
+        }
+        public void Clear() => entries.Clear();
+        public bool Contains(LeaderboardEntry item) => entries.Contains(item);
+        public void CopyTo(LeaderboardEntry[] array, int arrayIndex) => entries.CopyTo(array, arrayIndex);
+        public IEnumerator<LeaderboardEntry> GetEnumerator() => entries.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public int IndexOf(LeaderboardEntry item) => entries.IndexOf(item);
+        public void Insert(int index, LeaderboardEntry item)
+        {
+            throw new System.NotSupportedException("Insert is not supported");
+        }
+        public bool Remove(LeaderboardEntry item) => false; // no removal
+        public void RemoveAt(int index)
+        {
+            throw new System.NotSupportedException("RemoveAt is not supported");
+        }
+    }
+}
