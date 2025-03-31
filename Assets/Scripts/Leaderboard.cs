6daa8f35c13f1dc63d39494edb8079a196f1362a
Elia Cohen <mrc6113@rit.edu>
2025-03-31 11:41:18 -0400

    leaderboard setup

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -5,18 +5,54 @@ using System.Linq;
 
 public class Leaderboard : MonoBehaviour
 {
-    public int size = 10;
+    public List<LeaderboardItem> leaderboardItems;
     private LeaderboardList leaderboardList;
+    private LeaderboardEntry test = new LeaderboardEntry("test", "Test Player", 100);
+    private LeaderboardEntry test2 = new LeaderboardEntry("test2", "Test Player 2", 500);
+    private LeaderboardEntry test3 = new LeaderboardEntry("test3", "Test Player 3", 500);
+    private LeaderboardEntry test4 = new LeaderboardEntry("test4", "Test Player 4", 150);
+    private LeaderboardEntry test5 = new LeaderboardEntry("test5", "Test Player 5", 250);
+    private LeaderboardEntry test6 = new LeaderboardEntry("test6", "Test Player 6", 300);
+    private LeaderboardEntry test7 = new LeaderboardEntry("test7", "Test Player 7", 400);
+    private LeaderboardEntry test8 = new LeaderboardEntry("test8", "Test Player 8", 350);
+    private LeaderboardEntry test9 = new LeaderboardEntry("test9", "Test Player 9", 450);
+    private LeaderboardEntry test10 = new LeaderboardEntry("test10", "Test Player 10", 500);
+
 
     public void Start()
     {
-        leaderboardList = new LeaderboardList(size);
+        leaderboardList = new LeaderboardList(leaderboardItems.Count);
+        NewScore(test.id, test.playerName, test.score);
+        NewScore(test2.id, test2.playerName, test2.score);
+        NewScore(test3.id, test3.playerName, test3.score);
+        NewScore(test4.id, test4.playerName, test4.score);
+        NewScore(test5.id, test5.playerName, test5.score);
+        NewScore(test6.id, test6.playerName, test6.score);
+        NewScore(test7.id, test7.playerName, test7.score);
+        NewScore(test8.id, test8.playerName, test8.score);
+        NewScore(test9.id, test9.playerName, test9.score);
+        NewScore(test10.id, test10.playerName, test10.score);
     }
 
     public void NewScore(string id, string playerName, int score)
     {
         LeaderboardEntry entry = new LeaderboardEntry(id, playerName, score);
         leaderboardList.Add(entry);
+
+        // update the leaderboard UI
+        for (int i = 0; i < leaderboardItems.Count; i++)
+        {
+            if (i < leaderboardList.Count)
+            {
+                leaderboardItems[i].Name = leaderboardList[i].playerName;
+                leaderboardItems[i].Score = leaderboardList[i].score;
+                leaderboardItems[i].Visible = true;
+            }
+            else
+            {
+                leaderboardItems[i].Visible = false;
+            }
+        }
     }
 
     public List<LeaderboardEntry> GetLeaderboard()
---
+++ Assets/Scripts/LeaderboardItem.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class LeaderboardItem : MonoBehaviour
+{
+    public GameObject nameText;
+    public GameObject scoreText;
+    public string Name
+    {
+        set
+        {
+            nameText.GetComponent<TextMeshProUGUI>().text = value;
+        }
+    }
+    public int Score
+    {
+        set
+        {
+            scoreText.GetComponent<TextMeshProUGUI>().text = value.ToString() + "pts";
+        }
+    }
+
+    public bool Visible
+    {
+        set
+        {
+            gameObject.SetActive(value);
+        }
+    }
+}
