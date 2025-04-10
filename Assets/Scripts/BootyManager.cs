50a51543a07795a12c594fc199a747181cbd67cd
Andrew Black <acb7086@rit.edu>
2025-04-10 10:11:51 -0400

    updated model, fixed spawn issue
    gem spawns in when a midpoint is achieved, only doable when two rafts are spawned.
    
    testing is needed to ensure the gems spawn above the water
    
    model is fixed to use the proper gem model. the texture is the coin texture tinted red, so that could be implemented

--- Assets/Scripts/BootyManager.cs
+++ Assets/Scripts/BootyManager.cs
@@ -3,24 +3,40 @@ using System.Collections;
 using System.Linq.Expressions;
 using System;
 
+
+// Andrew Black
+// 4/8/25
+// BootyManager handles the spawning of Gems. It could be adpated for handling the coin spawns as well
+// hence the term 'booty' (pirate booty). 
+
 public class BootyManager : MonoBehaviour
 {
+    
 
     public GameObject gem;
 
+    //midpoint between two rafts
     private Vector3 midpoint;
 
+    // determine if a gem is ready to be spawned
     private bool readyToSpawn = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // for testing: fake midpoint so the gem spawns immediately
+        // midpoint = new Vector3(0, 1, 0); // set height so it's visible
+        // SpawnGem(); // spawn immediately
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        // check to ensure that the midpoint between two rafts isn't exactly 0,0,0
+        // this is sorta spaghetti and serves as an alternative to checking for a null value,
+        // as a Vector3 can't be null (defaults zero). If zero, and RNG rolls correctly,
+        // and we're ready to spawn, spawn a gem at the midpoint
         if((midpoint != Vector3.zero) && (UnityEngine.Random.Range(0,4) == 1) && readyToSpawn)
         {
             Debug.Log(midpoint);
@@ -28,6 +44,7 @@ public class BootyManager : MonoBehaviour
         }
     }
 
+    // this receives the positions of two flotsams, to determine the midpoint
     public void SendPositions(GameObject flotsam1, Vector3 flotsam2Pos)
     {
         Vector3 flotsam1Pos = flotsam1.transform.position;
@@ -35,11 +52,13 @@ public class BootyManager : MonoBehaviour
 
     }
 
+    // spawns the gem!
     private void SpawnGem()
     {
         Debug.Log("spawngem");
         Instantiate(gem, midpoint, Quaternion.identity);
-        StartCooldown();
+        StartCoroutine(StartCooldown());
+
     }
 
     private IEnumerator StartCooldown()
