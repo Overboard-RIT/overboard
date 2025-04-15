8548639acadfb81d94504e2a8d676e573cee237b
Andrew Black <acb7086@rit.edu>
2025-04-15 09:43:37 -0400

    backwall ui initial start
    mostly just understanding where the eff all the assets are

---
+++ Assets/Scripts/BackwallManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class BackwallManager : MonoBehaviour
+{
+    private int eventStep = 0;
+    private const int LAST_STEP = 9;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        eventStep = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (eventStep)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+        }
+
+    }
+
+
+}
